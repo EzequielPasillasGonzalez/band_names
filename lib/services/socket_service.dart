73d578646d9fbe4c1f53b64337a0193f782d2bbc
Ezequiel <ingepasillasg@gmail.com>
2026-06-29 09:49:17 -0600

    feat: conection with socket

---
+++ lib/services/socket_service.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+
+enum ServerStatus { online, offline, connecting }
+
+class SocketService with ChangeNotifier {
+  ServerStatus _serverStatus = ServerStatus.connecting;
+  late io.Socket _socket;
+
+  ServerStatus get serverStatus => _serverStatus;
+  io.Socket get socket =>
+      _socket; // <-- Por si necesitas usarlo en tus pantallas
+
+  SocketService() {
+    _initConfig();
+  }
+
+  void _initConfig() {
+    _socket = io.io(
+      'http://172.16.30.25:3000',
+      io.OptionBuilder()
+          .setTransports(['websocket'])
+          .enableAutoConnect()
+          .build(),
+    );
+
+    // Escuchar el evento de conexión de forma explícita
+    _socket.onConnect((_) {
+      _serverStatus = ServerStatus.online;
+      notifyListeners();
+    });
+
+    // Escuchar el evento de desconexión
+    _socket.onDisconnect((_) {
+      _serverStatus = ServerStatus.offline;
+      notifyListeners();
+    });
+
+    //  Escuchar errores de conexión
+    // _socket.onConnectError((data) => print('Error de conexión: $data'));
+    // _socket.onTypeError((data) => print('Error de tipo: $data'));
+  }
+}
